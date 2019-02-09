0a3090122b3e4a8fb31355e4a1fd8360bd391dc9
estebandalelr <estebandalelr@gmail.com>
2019-02-09 16:06:39 -0500

    Add ConnectionList

---
+++ front/src/ConnectionList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Table from '@material-ui/core/Table';
+
+class ConnectionList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <Table>
+                
+            </Table>
+         );
+    }
+}
+ 
+export default ConnectionList;

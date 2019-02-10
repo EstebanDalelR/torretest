6d00acf77f074e7c793034e500466c2e4ad3fc56
estebandalelr <estebandalelr@gmail.com>
2019-02-10 00:02:19 -0500

    Add routes to fetch and respond, app listener

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 
 const express = require('express')
 const app = express()
-const path = require('path');
 
 app.use('/', express.static('./front/build'))
 
-app.get("/getperson", (req, res)=> {
-  
+app.get('/getperson', (req, res) => {
+  let username = req.body.username
+  fetch(`https://torre.bio/api/bios/${username}`)
+    .then(resp => {
+      res.send(resp)
+    })
+    .catch(error => console.error('sorry, something went wrong:' + error))
 })
 
+app.get('/getconnections', (req, res) => {
+  let username = req.body.username
+  fetch(`https://torre.bio/api/people/${username}/connections?limit=20`)
+    .then(resp => {
+      res.send(resp)
+    })
+    .catch(error => console.error('sorry, something went wrong:' + error))
+})
+
+app.listen(3100)
 module.exports = app
